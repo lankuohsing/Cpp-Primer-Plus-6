9ed331dbdfb99b82603483efbe4e122fa1fec4e4
lankuohsing <lankuohsing@163.com>
2017-02-20 00:22:15 +0800

    数组，vector和array，未完待续

---
+++ chapter4/4-24_choices/4-24_choices/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>	//STL C++98
+#include <array>	//C++11
+int main ( )
+{
+	using namespace std;
+	//C, original C++
+	double a1 [ 4 ] = { 1.2, 2.4, 3.6, 4.8 };
+
+}
